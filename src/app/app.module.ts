2cf8f39252bf598ac1923296e0a702e0b8723790
Desimir Popović <110031217+dessa995@users.noreply.github.com>
2023-08-20 15:28:35 +0200

    animations and functionality update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './blocks/header/header.component';
@@ -12,7 +13,7 @@ import { BannerComponent } from './blocks/banner/banner.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, BannerComponent],
-  imports: [BrowserModule, FontAwesomeModule],
+  imports: [BrowserModule, FontAwesomeModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/blocks/header/header.component.ts
+++ src/app/blocks/header/header.component.ts
@@ -1,16 +1,50 @@
 import { Component } from '@angular/core';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.5s ease')
+      ]),
+      transition(':leave', [
+        animate('0.5s ease', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class HeaderComponent {
   isActive: boolean = false;
+  accordionActive: boolean = false;
+  accordionChildActive: boolean = false;
+  faCaretDown = faCaretDown;
+  faClose = faTimes;
+  accordionIcon: any = this.faCaretDown;
+  accordionChildIcon: any = this.faCaretDown;
 
   constructor() {}
 
   hamburgerClicked() {
     this.isActive = !this.isActive;
+  }
+
+  accordionClicked() {
+    this.accordionActive = !this.accordionActive;
+    if (window.innerWidth < 1200) {
+      this.accordionIcon = this.accordionActive ? this.faClose : this.faCaretDown;
+    }
+  }
+
+  accordionChildClicked() {
+    this.accordionChildActive = !this.accordionChildActive;
+    if (window.innerWidth < 1200) {
+      this.accordionChildIcon = this.accordionChildActive ? this.faClose : this.faCaretDown;
+    }
   }
 }
